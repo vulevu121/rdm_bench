5859236176279457a1b6b75b8b3adfdc8387a444
Vu Le <vle@karmaautomotive.com>
2018-07-30 12:01:36 -0700

    GUI added

---
+++ main.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton,QLineEdit
+from PyQt5.QtGui import QIcon, QPixmap
+import Shared
+import sys
+
+import design1
+
+
+class ExampleApp(QMainWindow, automotive.Ui_MainWindow):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+        icon = QIcon()
+        icon.addPixmap(QPixmap("icon.png"))
+        self.pushButton.setIcon(icon)
+        self.pushButton.clicked.connect(self.print_text)
+
+
+    def print_text(self):
+        soc = Shared.soc
+        self.label.setText("The SOC is {}".format(soc))
+        print('SOC:', soc)
+        
+		
+def main():
+    app = QApplication(sys.argv)
+    form = ExampleApp()
+    form.show()
+    app.exec_()
+	
+if __name__ == '__main__':
+    main()
