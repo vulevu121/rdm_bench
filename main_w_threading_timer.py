50dc46eeb34da2e13b96c8dd214006e89bb23e9e
Khuong Nguyen <Khuongnguyensac@gmail.com>
2018-09-14 18:52:18 -0700

    update

--- main_w_threading_timer.py
+++ main_w_threading_timer.py
@@ -112,6 +112,7 @@ class ExampleApp(QMainWindow, Ui_MainWindow):
     
         # Pop Up meassage box
         self.CAN_adapter_msg = QMessageBox()
+        self.CAN_adapter_msg.resize(400,300)
         self.CAN_adapter_msg.setIcon(QMessageBox.Critical)
         self.CAN_adapter_msg.setText('CAN bus can not be found.')
         self.CAN_adapter_msg.setInformativeText('Please check PEAK CAN adapter and try again.')
